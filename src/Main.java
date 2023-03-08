2be5bfd333129e5d92cc899839a0fca638652490
Elena <tvisterlenka@gmail.com>
2023-03-08 22:52:42 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,7 @@ public class Main {
 
 
     public static void main(String[] args) {
-/*
-Make (add code to class Tank),
-with fuel and different constructors
-*/
+
 
         // At (0;0) fuel=100
         Tank justTank = new Tank();
